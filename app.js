8bbdcb4ec86569efff2e7aa24468a2b57192a5bc
Mateus <mateusprocontact@gmail.com>
2017-10-11 14:32:36 -0300

    Brazbiss domain update

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.get('/', function(req, res){
 		let googleDescription = '<meta name="description" content="Encontre empresas brasileiras nos EUA e negocie com a comunidade brasileira nos Estados Unidos da América." />'
 		let googleMedias = '<script type="application/ld+json">{"@context": "http://schema.org","@type": "Person","name": "Brazbiss","url": "http://www.brazbiss.com","sameAs": ["https://www.facebook.com/Brazbiss-1568609906532929/","https://medium.com/brazbiss"]}</script>'
 		let facebook = '<meta property="og:url" content="http://www.brazbiss.com" /><meta property="og:type" content="article" /><meta property="og:title" content="Encontre empresas brasileiras nos EUA | Brazbiss.com<" /><meta property="og:description" content="Encontre empresas brasileiras nos EUA e negocie com a comunidade brasileira nos Estados Unidos da América." /><meta property="og:image" content="https://i.imgur.com/F7scg9x.png" />'
-		let linkedIn = '<meta property="og:title" content="Encontre empresas brasileiras nos EUA." /><meta property="og:description" content="Encontre empresas brasileiras nos EUA e negocie com a comunidade brasileira nos Estados Unidos da América." /><meta property="og:url" content="http://brazbiss.com/" /><meta property="og:image" content="https://i.imgur.com/F7scg9x.png" />'
+		let linkedIn = ''
 
 		res.render('pages/index',{title: 'Encontre empresas brasileiras nos EUA.', places: results, metatags: {google: googleDescription, googleMedias: googleMedias, facebook: facebook, linkedin: linkedIn}})
 	})
@@ -29,13 +29,22 @@ app.get('/', function(req, res){
 })
 //contact
 app.get('/contact', function(req, res){
-	res.render('pages/contact',{title: ''})
+	let googleDescription = '<meta name="description" content="Encontre empresas brasileiras nos EUA e negocie com a comunidade brasileira nos Estados Unidos da América." />'
+	let googleMedias = '<script type="application/ld+json">{"@context": "http://schema.org","@type": "Person","name": "Brazbiss","url": "http://www.brazbiss.com","sameAs": ["https://www.facebook.com/Brazbiss-1568609906532929/","https://medium.com/brazbiss"]}</script>'
+	let facebook = '<meta property="og:url" content="http://www.brazbiss.com" /><meta property="og:type" content="article" /><meta property="og:title" content="Encontre empresas brasileiras nos EUA | Brazbiss.com<" /><meta property="og:description" content="Encontre empresas brasileiras nos EUA e negocie com a comunidade brasileira nos Estados Unidos da América." /><meta property="og:image" content="https://i.imgur.com/F7scg9x.png" />'
+	let linkedIn = ''
+
+	res.render('pages/contact',{title: 'Anuncie empresas brasileiras nos EUA.',metatags: {google: googleDescription, googleMedias: googleMedias, facebook: facebook, linkedin: linkedIn}})
 })
 //results
 app.all('/search/:state/:city/:category/:page*?', function(req, res){
 	let Companies = require('./models/Companies.model')
 	Companies.find({state:req.params.state,city:req.params.city,category:req.params.category},{'_id': 0},{sort: {name: 1}}, function(err,results){
-		res.render('pages/search',{title: req.params.category+' | '+req.params.city, state: req.params.state, city: req.params.city, category: req.params.category, companies: results,  metatags: {}})
+		let googleDescription = '<meta name="description" content="Encontre empresas brasileiras nos EUA e negocie com a comunidade brasileira nos Estados Unidos da América." />'
+		let googleMedias = '<script type="application/ld+json">{"@context": "http://schema.org","@type": "Person","name": "Brazbiss","url": "http://www.brazbiss.com","sameAs": ["https://www.facebook.com/Brazbiss-1568609906532929/","https://medium.com/brazbiss"]}</script>'
+		let facebook = '<meta property="og:url" content="http://www.brazbiss.com" /><meta property="og:type" content="article" /><meta property="og:title" content="Encontre empresas brasileiras nos EUA | Brazbiss.com" /><meta property="og:description" content="Encontre empresas brasileiras nos EUA e negocie com a comunidade brasileira nos Estados Unidos da América." /><meta property="og:image" content="https://i.imgur.com/F7scg9x.png" />'
+		let linkedIn = ''
+		res.render('pages/search',{title: req.params.category+' | '+req.params.city, state: req.params.state, city: req.params.city, category: req.params.category, companies: results,  metatags: {google: googleDescription, googleMedias: googleMedias, facebook: facebook, linkedin: linkedIn}})
 
 	})
 })
@@ -43,7 +52,10 @@ app.all('/search/:state/:city/:category/:page*?', function(req, res){
 app.get('/page/:friendlyUrl', function(req, res){
 	let Companies = require('./models/Companies.model')
 	Companies.findOne({url:req.params.friendlyUrl},{'_id': 0},{sort: {name: 1}}, function(err,results){
-		res.render('pages/page',{title: results.name + ' | ' + results.city + ' | ' + results.state || '', company: results, metatags: {}})
+		let googleDescription = '<meta name="description" content="'+results.metatags.googleDescription+'" />'
+		let facebook = '<meta property="og:url" content="http://www.brazbiss.com/page/'+results.url+'" /><meta property="og:type" content="article" /><meta property="og:title" content="'+results.metatags.facebookTitle+'" /><meta property="og:description" content="'+results.metatags.facebookDescription+'" /><meta property="og:image" content="'+results.medias.image+'" />'
+		let linkedIn = ''
+		res.render('pages/page',{title: results.name + ' | ' + results.city + ' | ' + results.state || '', company: results, metatags: {google: googleDescription, facebook: facebook, linkedin: linkedIn}})
 	})
 	
 })
--- public/script-search.js
+++ public/script-search.js
@@ -3,11 +3,11 @@ $(document).ready(function(){
 	let state = window.location.pathname.split('/')[2] || 'Massachusetts'
 	let city = window.location.pathname.split('/')[3] || 'Boston city'
 	let category = window.location.pathname.split('/')[4] || 'Restaurantes'
-	let host = 'http://'+window.location.host
+	let host = 'http://brazbiss.com'
 
 	for(let i = 0;i<companies.length;i++){
 		let html = '<div class="col-md-8 item-result">'
-		html += '<div class="item-result-title"><a href="http://'+window.location.host+'/page/'+companies[i].url+'">'+companies[i].name+'</a></div>'
+		html += '<div class="item-result-title"><a href="http://brazbiss.com/page/'+companies[i].url+'">'+companies[i].name+'</a></div>'
 		html += '<div class="item-result-intro">'
 		html += companies[i].intro
 		html += '</div>'
@@ -15,7 +15,7 @@ $(document).ready(function(){
 		html += companies[i].address
 		html += '</div>'
 		html += '<div class="item-result-button">'
-		html += '<a href="http://'+window.location.host+'/page/'+companies[i].url+'" class="btn">Mais Informações</a>'
+		html += '<a href="http://brazbiss.com/page/'+companies[i].url+'" class="btn">Mais Informações</a>'
 		html += '</div>'
 		html +='</div>'
 		$('.search-results').append(html)
